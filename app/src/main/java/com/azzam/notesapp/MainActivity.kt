40b8bd4c28f4f0cb47dc4a6773d44ebb43bbc018
Muhammad Azzam <MazzamR08@gmail.com>
2022-02-28 15:10:46 +0700

    going on

--- app/src/main/java/com/azzam/notesapp/MainActivity.kt
+++ app/src/main/java/com/azzam/notesapp/MainActivity.kt
@@ -2,11 +2,28 @@ package com.azzam.notesapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         supportActionBar?.elevation = 0F
+
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.nav_host_fragment_container) as NavHostFragment?
+
+        if (navHostFragment != null) {
+            val appBarConfiguration = AppBarConfiguration(navHostFragment.navController.graph)
+            setupActionBarWithNavController(navHostFragment.navController, appBarConfiguration)
+        }
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        val navController = findNavController(R.id.nav_host_fragment_container)
+        return super.onSupportNavigateUp() || navController.navigateUp()
     }
 }
